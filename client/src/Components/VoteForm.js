ab9876b4e9896cfa17d5b9030cc71aa2c2624bf2
Ali Mujtaba <am.alimujtaba@gmail.com>
2021-06-23 10:23:56 +0530

    Remove code that is not required.

--- client/src/Components/VoteForm.js
+++ client/src/Components/VoteForm.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 const VoteForm = ({list,onVote})=>{
     console.log("Voteform: ",list);
     console.log("onVote:",onVote);
-    const onSubmit = (e) => {
-        e.preventDefault();
-        onVote(e);
-    };
     return (
-        <Form onSubmit={onSubmit}>
+        <Form>
             <Form.Group controlId="candidateSelect">
                 <Form.Label> Choose a candidate </Form.Label>
                 <Form.Control as="select" onChange={onVote}>
