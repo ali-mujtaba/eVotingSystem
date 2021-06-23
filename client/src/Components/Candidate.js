ae0c5c0aaea2101c8ea97876a540a8d1c72439f1
Ali Mujtaba <am.alimujtaba@gmail.com>
2021-06-23 09:28:52 +0530

    Modify Candidate and Candidate List components to use react-bootstrap components.

--- client/src/Components/Candidate.js
+++ client/src/Components/Candidate.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Candidate = ({id, name, voteCount})=>{
     return (
-        <tr className="candidate-info"><th> {id} </th><td> {name} </td><td> {voteCount} </td></tr>
+        <tr>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{voteCount}</td>
+        </tr>
     );
 };
 
--- client/src/Components/CandidatesList.js
+++ client/src/Components/CandidatesList.js
@@ -1,18 +1,28 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 import Candidate from "./Candidate";
-
+import Container from "react-bootstrap/Container";
 const CandidatesList = ({list})=>{
     // console.log(list);
     return (
-        <div className="candidate-list-container">
-            <tbody className="candidate-info-table">    
-                {
-                    list.map(candidate => (
-                        <Candidate id = {candidate[0]} name={candidate[1]} voteCount={candidate[2]}/>            
-                    ))
-                }
-            </tbody>
-        </div>
+        <Container fluid style={{display:"grid", textAlign:"center"}}>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>Candidate Name</th>
+                    <th>Votes</th>
+                    </tr>
+                </thead>
+                <tbody style={{textAlign:"center"}}>
+                    {
+                        list.map(candidate => (
+                            <Candidate id = {candidate[0]} name={candidate[1]} voteCount={candidate[2]}/>            
+                        ))
+                    }
+                </tbody>
+            </Table>
+        </Container>
     );
 };
 
